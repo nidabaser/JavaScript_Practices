42c200ef655d9f67bc2d9473ad062fca2c2f763c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-06 23:48:44 +0300

    installing extensions for VS Code

---
+++ 1.Eklentilerin Kurulmasi/app.js
@@ -0,0 +1,25 @@
+// Better Comments Extension
+// * Vurgulu
+// ? Bu bir soru
+// ! Önemli, kontrol et
+// TODO: Buraya if koşulu eklenecek
+
+/*
+* JavaScript Console Utils Extension Shortcuts
+* CTRL + SHIFT + D --> Remove all console.logs
+* CTRL + SHIFT + L --> Output (on the same line) console.log();
+*/
+console.log("Selam Console !");
+console.log();console.log();console.log();
+
+// Javascript (ES6) code snippets extension:
+// This extension contains code snippets for JavaScript in ES6 syntax for Vs Code editor
+
+// Auto Rename Tag extension:
+// Automatically rename paired HTML/XML tag, same as Visual Studio IDE does.
+
+// Git Graph Extension
+// View a Git Graph of your repository, and easily perform Git actions from the graph.
+
+// Live Server Extension
+// Launch a development local Server with live reload feature for static & dynamic pages.
