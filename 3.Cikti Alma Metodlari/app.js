00bb4e4f1dadbc259b10d215829ed51c9fc0327f
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-07 00:29:29 +0300

    output methods (document.write, console.log, alert popup) in js

---
+++ 3.Cikti Alma Metodlari/app.js
@@ -0,0 +1,24 @@
+// Document write ile çıktı verme
+document.writeln("Hello World <br>");
+document.writeln("Selam Dunya");
+document.writeln(333);
+console.log(true);
+
+// Console log metodu
+console.log("Hi JavaScript");
+console.log("I'm Backend developer");
+console.log(3.33);
+console.log({name:"Alice",surname:"Wonder"});
+let a=5;
+let b=10;
+console.log("Sum of two numbers: ", a+b);
+console.clear();
+
+// // 2 tane // //  yazınca deprecated
+
+// Alert (Popup ile cikti verme islemleri)
+//console.log(window);
+//alert("Popup!")
+let x=5;
+let y=10;
+alert("Sum of two numbers: " + (x + y));
