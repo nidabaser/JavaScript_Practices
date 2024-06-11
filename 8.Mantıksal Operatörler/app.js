db328e98130e0adcb9d0060e023439dab45a2bb5
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-11 18:28:59 +0300

    logical operators

---
+++ 8.Mantıksal Operatörler/app.js
@@ -0,0 +1,17 @@
+// Logical Operators
+
+/*
+
+(&&) : AND
+(||) : OR
+(!)  : NOT
+
+*/
+
+let age = 25;
+let money = 10000;
+let isHealthy = true;
+
+console.log(age>18 && money>5000 || isHealthy==true);
+
+console.log(!(5>3)); // NOT Operator
