4d1a58fd8c9abcc4fe7ab323e245665909c07bac
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-11 18:40:12 +0300

    comparison operators

---
+++ 9.Karşılaştırma Operatörleri/app.js
@@ -0,0 +1,21 @@
+// Comparison Operators
+
+/*
+
+(!=) : is Not Equal
+(>)  : büyük mü
+(<)  : küçük mü
+(>=) : büyük eşit mi
+(<=) : küçük eşit mi
+
+*/
+
+console.log(5!=3);
+
+console.log(15<22);
+
+console.log(15>22);
+
+console.log(11<=11); // --> [ 11<11 || 11==11 ]
+
+console.log(12>=13); // --> [ 12>13 || 12==13 ]
