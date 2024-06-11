15613ba87806fc41d6d4ad33db03c5345bd1e621
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-11 18:07:00 +0300

    aritmetic operators

---
+++ 6.Aritmetik Operatörler/app.js
@@ -0,0 +1,31 @@
+// Aritmetic Operators
+
+/*
+(=) : atama
+(+) : arttırma
+(-) : azaltma
+(*) : çarpma
+(/) : bölme
+(%) : mod alma / kalanı bulma
+(++) : 1 artırma
+(--) : 1 azaltma
+(**) : üs alma
+*/
+
+let point1 = 50;
+let point2 = 100;
+console.log("Sum of Points: " + point1 + point2); // String
+console.log("Sum of Points: " + (point1 + point2)); // Sum of Strings
+
+// "var" yerine "let" kullanımı öneriliyor, bellek tasarrufu için
+
+let a = 10%3;
+console.log(a);
+
+let b = 10;
+b++;
+console.log(b);
+b--;
+console.log(b);
+
+console.log(2**3);
