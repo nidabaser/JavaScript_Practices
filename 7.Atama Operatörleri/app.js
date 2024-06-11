991460905b8f466eed621ac5a9b5c4f23e7c8c39
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-11 18:19:03 +0300

    assigment operators

---
+++ 7.Atama Operatörleri/app.js
@@ -0,0 +1,31 @@
+// Assignment Operators
+
+/*
+
+= : atama
+== : eşit mi?
+=== : denk mi?
++=
+-=
+*=
+/=
+%=
+**=
+
+*/
+
+let a = 5;
+let b= 10;
+console.log(5 == a); // True or False
+console.log(3 == "3"); // Values are equal !
+console.log(3 === "3"); // Data types are not equal !
+
+let num = 4;
+num += 2;
+console.log(num);
+num *= 3;
+console.log(num);
+num %= 5;
+console.log(num);
+num **= 3;
+console.log(num);
