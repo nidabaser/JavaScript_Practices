ba27c362efb7025d197e3fc49777f14736687c7e
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-12 21:06:27 +0300

    basic examples

--- 11.Koşul Yapıları/app.js
+++ 11.Koşul Yapıları/app.js
@@ -1,10 +1,21 @@
 // Conditions
 
-let point = 55;
+// let point = 55;
 
-if(point>50){
-    console.log("Passed, your point: ", point);
+// if(point>50){
+//     console.log("Passed, your point: ", point);
+// } else {
+//     console.log("Not Passed, your point: ", point);
+// }
+
+let age = Number(prompt("Your age: "));
+console.log(typeof age);
+
+let money = Number(prompt("Budget: "));
+console.log(typeof money);
+
+if(age>=18 && money>=3000 ){
+    alert("You can enter driving test");
 } else {
-    console.log("Not Passed, your point: ", point);
+    alert("You can not enter driving test");
 }
-
