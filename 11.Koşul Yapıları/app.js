2363c45cae1d30410db4ffafdb74c4fc64d0029c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-13 01:10:12 +0300

    basic example for checking ID and username with conditions

--- 11.Koşul Yapıları/app.js
+++ 11.Koşul Yapıları/app.js
@@ -1,59 +1,78 @@
 // Conditions
 
-// let point = 55;
+/*
+let point = 55;
+if(point>50){
+    console.log("Passed, your point: ", point);
+} else {
+    console.log("Not Passed, your point: ", point);
+}
+*/
 
-// if(point>50){
-//     console.log("Passed, your point: ", point);
-// } else {
-//     console.log("Not Passed, your point: ", point);
-// }
-
-// let age = Number(prompt("Your age: "));
-// console.log(typeof age);
-
-// let money = Number(prompt("Budget: "));
-// console.log(typeof money);
-
-// if(age>=18 && money>=3000 ){
-//     alert("You can enter driving test");
-// } else {
-//     alert("You can not enter driving test");
-// }
-
+/*
+let age = Number(prompt("Your age: "));
+console.log(typeof age);
+let money = Number(prompt("Budget: "));
+console.log(typeof money);
+if(age>=18 && money>=3000 ){
+    alert("You can enter driving test");
+} else {
+    alert("You can not enter driving test");
+}
+*/
 
 // GPA Calculation
-// exam1 (%30), exam2 (%30), finalExam (%40)
-// let exam1 = Number(prompt("Enter your first point: "));
-// let exam2 = Number(prompt("Enter your second point: "));
-// let finalExam = Number(prompt("Enter your final exam point: "));
-// let average = ((exam1 * 0.3) + (exam2 * 0.3) + (finalExam * 0.4));
+/*
+exam1 (%30), exam2 (%30), finalExam (%40)
+let exam1 = Number(prompt("Enter your first point: "));
+let exam2 = Number(prompt("Enter your second point: "));
+let finalExam = Number(prompt("Enter your final exam point: "));
+let average = ((exam1 * 0.3) + (exam2 * 0.3) + (finalExam * 0.4));
 
-// if (average >= 60) {
-//     alert("Congrats, You have passed :) Your GPA: " + average);
-//     console.log("Dersten geçtiniz, tebrikler!");
-// } else {
-//     alert("Oh no, You have not passed :( Your GPA: " + average);
-//     console.log("Dersten kaldiniz, geçmiş olsun :(");
-// }
+if (average >= 60) {
+    alert("Congrats, You have passed :) Your GPA: " + average);
+    console.log("Dersten geçtiniz, tebrikler!");
+} else {
+    alert("Oh no, You have not passed :( Your GPA: " + average);
+    console.log("Dersten kaldiniz, geçmiş olsun :(");
+}
+*/
 
 // Yol Ayrımı Uygulaması
-
+/*
 let chosenPath = prompt("Please choose the way you want to go: [1] or [2] or [3] ");
-
 if (chosenPath == 1) {
-
     alert("You have chosen " + chosenPath + ". path");
-
 } else if (chosenPath == 2){
-
     alert("You have chosen " + chosenPath + ". path");
-    
 } else if (chosenPath == 3) {
-
     alert("You have chosen " + chosenPath + ". path");
-
 } else {
-
     alert("Please choose valid path");
-    
+}
+*/
+
+
+// Çoklu if kullanmak
+
+/*
+Kullanıcı adı, boş olamaz
+ID no 11 karakterden oluşacak
+*/
+
+let idNo = prompt("Enter your id number: ");
+let username = prompt("Enter your name: ");
+
+checkIdAndName(idNo, username);
+
+function checkIdAndName (id, name){
+    if (name != "") {
+        if (id.length == 11) {
+            console.log("Success");
+        } else {
+            console.log("Please enter valid ID number");
+        }
+    } else {
+        console.log("Username can not be null");
+    }
 }
