4ba21382c1548b46a8bcb8b6d426d179c6a6ddca
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-12 21:27:57 +0300

    gpa calculation example

--- 11.Koşul Yapıları/app.js
+++ 11.Koşul Yapıları/app.js
@@ -8,14 +8,30 @@
 //     console.log("Not Passed, your point: ", point);
 // }
 
-let age = Number(prompt("Your age: "));
-console.log(typeof age);
+// let age = Number(prompt("Your age: "));
+// console.log(typeof age);
 
-let money = Number(prompt("Budget: "));
-console.log(typeof money);
+// let money = Number(prompt("Budget: "));
+// console.log(typeof money);
 
-if(age>=18 && money>=3000 ){
-    alert("You can enter driving test");
+// if(age>=18 && money>=3000 ){
+//     alert("You can enter driving test");
+// } else {
+//     alert("You can not enter driving test");
+// }
+
+
+// GPA Calculation
+// exam1 (%30), exam2 (%30), finalExam (%40)
+let exam1 = Number(prompt("Enter your first point: "));
+let exam2 = Number(prompt("Enter your second point: "));
+let finalExam = Number(prompt("Enter your final exam point: "));
+let average = ((exam1 * 0.3) + (exam2 * 0.3) + (finalExam * 0.4));
+
+if (average >= 60) {
+    alert("Congrats, You have passed!");
+    console.log("Dersten geçtiniz, tebrikler!");
 } else {
-    alert("You can not enter driving test");
+    alert("Oh no, You have not passed :(");
+    console.log("Dersten kaldiniz, geçmiş olsun :(");
 }
