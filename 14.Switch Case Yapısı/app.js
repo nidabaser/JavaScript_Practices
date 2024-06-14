59221a88cd4c368af9d930bf0f143f3a15b84dc1
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-14 23:03:09 +0300

    switch-case example

---
+++ 14.Switch Case Yapısı/app.js
@@ -0,0 +1,34 @@
+// Switch Case Kullanımı
+
+let num = prompt("Enter number: ");
+
+switch(num){
+    case "1":
+        alert("Monday");
+        console.log("Number is 1");
+        break;
+    case "2":
+        alert("Tuesday");
+        console.log("Number is 2");
+        break;
+    case "3":
+        alert("Wednesday");
+        console.log("Number is 3");
+        break;
+    case "4":
+        alert("Thursday");
+        console.log("Number is 4");
+        break;
+    case "5":
+        alert("Friday");
+        console.log("Number is 5");
+        break;
+    case ("6"):
+        alert("Saturday");
+        console.log("Number is 6");
+        break;
+    case "7":
+        alert("Sunday");
+        console.log("Number is 7");
+        break;
+}
