c7458e58abf5fd47db2e5c52aaaa3fdd0a4b9026
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-15 23:30:20 +0300

    atm example

---
+++ 15.ATM uygulaması/app.js
@@ -0,0 +1,50 @@
+/*
+    ATM Uygulaması
+1. Bakiye görüntüleme
+2. Para çekme
+3. Para yatırma
+4. Çıkış
+
+*/
+
+let newLine = "\r\n";
+let balance = 1000;
+let menuText = "1.Display balance" + newLine
++ "2.Withdraw" + newLine
++ "3.Deposit" + newLine
++ "4.Exit" + newLine;
+
+let select = prompt(menuText);
+
+switch (select) {
+
+    case "1":
+        alert("Balance : " + balance);
+    break;
+
+    case "2":
+        let withdrawAmount = Number(prompt("Enter withdraw amount: "));
+
+        if (withdrawAmount <= balance) {
+            balance -= withdrawAmount;
+        } else {
+            alert("Error because " + newLine + "Your balance: " + balance
+                + " is smaller than your withdraw amount!");
+        }
+    break;
+    
+    case "3":
+        let depositAmount = Number(prompt("Enter deposit amount: "));
+        balance += depositAmount;
+        alert("Your current balance: " + balance);
+    break;
+
+    case "4":
+        console.log("Exit");
+    break;
+
+    default:
+        console.log("Please select valid option [1,2,3,4]");
+    break;
+        
+}
