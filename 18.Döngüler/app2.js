ec1677b612b2c90b01936d7cd76cc6e83604cc09
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-18 00:52:34 +0300

    while loop examples

---
+++ 18.Döngüler/app2.js
@@ -0,0 +1,37 @@
+// While Loop Examples
+
+// 1 to 10
+// let count = 1;
+// while (count <= 10) {
+//     console.log(count);
+//     count++; // If you don't write this line, it will be forever loop :D
+// }
+
+// 1 to 10 Even Numbers
+// let count = 0;
+// while (count <= 10) {
+//     if (count % 2 == 0) {
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// 1 to 10 Odd Numbers
+// let count = 1;
+// while (count <= 10) {
+//     if (count % 2 == 1) {
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// while(true)
+// let count = 1;
+// while (true) {
+//     console.log(count);
+//     if (count == 7) { // Output: 1,2,3,4,5,6,7.
+//         break;
+//     }
+//     count++;
+// }
+
