2d2c93bdf57731c8412fb1503e123118fcf46dd9
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-19 23:18:26 +0300

    multiplication table example

---
+++ 19.Çarpım Tablosu/app.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Multiplication Table Example
+ * 
+ * 1 x 1 = 1
+ * 1 x 2 = 2
+ * 1 x 3 = 3
+ * ...
+ * ..
+ * .
+ * 
+ */
+
+for(let i = 1; i<=10; i++){
+
+    for(let j = 1; j<=10; j++){
+
+        console.log(i + " x " + j + " = " + ( i * j ));
+
+    }
+
+    console.log("-----------------------------");
+    
+}
