ea2c059e6ebfefa9aa0405507782cd4bcf6bb2a9
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-21 18:44:54 +0300

    method examples without parameter and with parameter

---
+++ 23.Metodlar/app.js
@@ -0,0 +1,41 @@
+// ? Methods
+
+// ? Parametresiz ve geriye değer döndürmeyen
+
+// function printName(){
+//     console.log("Your name");
+// }
+// printName();
+
+// function toplama(){
+//     console.log(5+7);
+// }
+// toplama();
+
+// ? Parametreli ve geriye değer döndürmeyen
+
+// function yazdir(name, surname){
+//     console.log(name + " " + surname);
+// }
+// yazdir("Nida", "Başer");
+// yazdir("John", "Doe");
+// yazdir("Lorem", "Ipsum");
+
+// function cube(num){
+//     console.log(num*num*num);
+// }
+// cube(3);
+// cube(5);
+// cube(10);
+
+// let age = Number(prompt("Enter your age: "));
+// checkAge(age);
+
+// function checkAge(age){
+//     if(age>=18){
+//         console.log("Your age is enough to take drivers license");
+//     }else{
+//         console.log("Your age is NOT enough to take drivers license");
+//     }
+// }
+
