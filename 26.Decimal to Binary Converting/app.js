f7710c4989355c37206caaffb4975342f459415e
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-23 15:42:47 +0300

    add decimal to binary conversion function example

---
+++ 26.Decimal to Binary Converting/app.js
@@ -0,0 +1,43 @@
+// Decimal to Binary Converter Example
+
+convertDecimalToBinary(7);
+
+function convertDecimalToBinary(number){
+
+    let binary = "";
+
+    while(true){
+
+        binary += (number%2).toString();
+    
+        number = Math.floor(number / 2);
+
+        if(number == 1){
+
+            binary+=1;
+            
+            break;
+
+        }
+
+    }
+
+    let result = reverse(binary);
+
+    console.log("Result: " + result);
+
+}
+
+function reverse(binary){
+
+    let reverseBinary = "";
+
+    for(let i = binary.length-1; i >= 0; i--){
+
+        reverseBinary += binary.charAt(i);
+        
+    }
+
+    return reverseBinary;
+
+}
