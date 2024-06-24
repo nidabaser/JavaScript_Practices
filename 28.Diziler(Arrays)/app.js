5426ba93e671c3cc1d4f69f8b8a549a5ca84bf48
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-24 23:31:14 +0300

    array examples

---
+++ 28.Diziler(Arrays)/app.js
@@ -0,0 +1,19 @@
+// Diziler (Arrays)
+
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+
+let mixedArray = [0,1,2,3,"Nida",4,"A"];
+
+console.log(numbers[6]);
+
+mixedArray[4] = "Hello";
+
+console.log(mixedArray[4]);
+
+console.log(numbers[numbers.length-1]);
+
+numbers[numbers.length-1] = "Something";
+
+console.log(numbers);
+
+console.log(mixedArray);
