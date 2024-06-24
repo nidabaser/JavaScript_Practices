e3ef1cac57735d2fa8555d2337b8afe75f64e95d
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-24 23:53:34 +0300

    array examples, mixed arrays, defining a new array

--- 28.Diziler(Arrays)/app.js
+++ 28.Diziler(Arrays)/app.js
@@ -17,3 +17,27 @@ numbers[numbers.length-1] = "Something";
 console.log(numbers);
 
 console.log(mixedArray);
+
+let mixedArray2 = [0,1,"N","Hello",true,null,undefined];
+
+let newArray = [];
+
+let newArray2 = new Array(); // Bu yöntemle nesne türetmiş oluyoruz
+// Nesne türetmeninin performans maliyeti var, o yüzden üstteki yöntem daha iyi
+
+let personel1 = {
+    name: "Nida",
+    surname: "Başer",
+    age:25,
+}
+
+let personel2 = {
+    name: "Sena",
+    surname: "Başer",
+    age:23,
+}
+
+let personelArray = [personel1,personel2,1,true,numbers];
+console.log(personelArray[4][5]);
+console.log(personelArray[0].name);
+console.log(typeof personelArray);
