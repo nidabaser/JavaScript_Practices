aa4b8ce6948f7e32575f034c67ff0790b16d820e
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-25 00:29:08 +0300

    foreach loop example with arrays

---
+++ 29.Foreach metodu/app.js
@@ -0,0 +1,17 @@
+let names = ["Alice", "Bob", "John", "Cloud", "Aerith", "Tifa"];
+
+names.forEach(function(name){
+
+    console.log(name);
+
+});
+
+// Or we can use for loop to obtain exact same result
+
+for(let i = 0; i < names.length; i++){
+
+    console.log(names[i]);
+
+}
+
+// But using foreach loop for arrays is more chic :D
