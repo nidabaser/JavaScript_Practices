ac49881b7b1a486960df92c69128732a6d7f02e1
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-29 21:09:19 +0300

    add Math methods: floor, ceil and round examples

---
+++ 34.Math Metodları/app.js
@@ -0,0 +1,18 @@
+//Math Class Methods
+
+//Floor
+//Returns the greatest integer less than or equal to its numeric argument
+let a = 3.15;
+console.log(Math.floor(a));
+
+//Ceil
+//Returns the smallest integer greater than or equal to its numeric argument.
+let b = 3.01;
+console.log(Math.ceil(b));
+
+//Round
+//Returns a supplied numeric expression rounded to the nearest integer.
+let roundDown = 3.49;
+console.log(Math.round(roundDown));
+let roundUp = 3.50;
+console.log(Math.round(roundUp));
