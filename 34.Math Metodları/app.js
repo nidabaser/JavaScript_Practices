505d3d43e61313f478a67c1a2c8d1dcb092e9894
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-29 21:12:42 +0300

    add min, max method examples

--- 34.Math Metodları/app.js
+++ 34.Math Metodları/app.js
@@ -16,3 +16,12 @@ let roundDown = 3.49;
 console.log(Math.round(roundDown));
 let roundUp = 3.50;
 console.log(Math.round(roundUp));
+
+//Max
+//Returns the larger of a set of supplied numeric expressions.
+console.log(Math.max(1,2,3,55));
+
+//Min
+//Returns the smaller of a set of supplied numeric expressions.
+console.log(Math.min(10,0,-5));
+
