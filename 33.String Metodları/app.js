2956b82e311ddc66136419efe3f33c4f2872d861
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-29 21:00:55 +0300

    add String class methods examples : charAt, concat, indexOf, trim, slice, substring, replace, split etc...

---
+++ 33.String Metodları/app.js
@@ -0,0 +1,68 @@
+/**
+ * String Sınıfının Metodları
+ * 
+ * charAt()
+ * concat()
+ * indexOf()
+ * lastindexof()
+ * toUpperCase()
+ * toLowerCase()
+ * trim()
+ * slice()
+ * substring()
+ * replace()
+ * split()
+ * valueOf()
+ * startsWith()
+ * endsWith()
+ * 
+ */
+
+let course = "Modern JavaScript Development Course";
+let date = " 2024";
+
+// //charAt()
+// let character = course.charAt(1);
+// console.log(character);
+
+// //concat()
+// let result = course.concat(date);
+// console.log(result);
+
+// //indexOf()
+// let index = course.indexOf("S");
+// console.log(index);
+
+// //lastindexOf()
+// let lastIndex = course.lastIndexOf("Develop");
+// console.log(lastIndex);
+
+// //trim()
+// console.log(course.trim());
+
+// //slice
+// console.log(course.slice(12,15));
+
+// //substring()
+// console.log(course.substring(0,5));
+
+// //replace
+// console.log(course);
+// console.log(course.replace("JavaScript", "Java"));
+
+// //split()
+// console.log(course);
+// console.log(course.split(" ")); // or split(","); etc
+
+// //valueOf()
+// console.log(course.valueOf());
+
+// //startsWith()
+// console.log(course);
+// console.log(course.startsWith("M")); // True
+// console.log(course.startsWith("m")); // False
+
+// //ensWith()
+// console.log(course);
+// console.log(course.endsWith("e")); // True
+// console.log(course.endsWith("Course")); // True
