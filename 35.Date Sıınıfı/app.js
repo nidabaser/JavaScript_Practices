0abf9d16f37ab6132e82139959306b1f778d7f91
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-06-30 22:54:12 +0300

    add get methods for date class

---
+++ 35.Date Sıınıfı/app.js
@@ -0,0 +1,26 @@
+// Date Kullanımı
+
+let date = new Date();
+
+console.log(date);
+
+console.log(date.getFullYear());
+
+console.log(date.getDate());
+
+console.log(date.getDay());
+
+console.log(date.getHours());
+
+console.log(date.getMilliseconds());
+
+console.log(date.getMonth() + 1); // Jan = 0, Feb = 1 ...
+
+console.log(date.getSeconds());
+
+console.log(date.toLocaleDateString()); // Formatted date m/d/y
+
+console.log(date.toLocaleTimeString()); // Formatted time h:m:s AM/PM
+
+console.log(date.toLocaleString()); // Both date and time
+
