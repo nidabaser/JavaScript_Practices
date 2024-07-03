f7813ba76800f923e0f1e8acbb2797e7b7eeaf3e
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-03 23:43:23 +0300

    add document object | DOM: charSet, title, links, item, attribute, classList, children and content type

--- 37.Document Objesi DOM/app.js
+++ 37.Document Objesi DOM/app.js
@@ -6,14 +6,42 @@ let value;
 
 // console.log(document);
 
-value = document.location.href;
-console.log("href: " + value);
-value = document.location.hostname;
-console.log("hostname: " + value);
-value = document.location.port;
-console.log("port: " + value);
-value = document.location.pathname;
-console.log("pathname: " + value);
+// value = document.location.href;
+// console.log("href: " + value);
+// value = document.location.hostname;
+// console.log("hostname: " + value);
+// value = document.location.port;
+// console.log("port: " + value);
+// value = document.location.pathname;
+// console.log("pathname: " + value);
 
 // value = document.all; // deprecated
 
+value = document.characterSet;
+console.log("charset: " + value);
+
+value = document.title;
+console.log("title: " + value);
+
+value = document.links.item(4);
+console.log("links: " + value);
+
+value = document.links.item(4).id;
+console.log("link item: " + value);
+
+value = document.links.item(4).getAttribute("id");
+console.log("link item attribute id: " + value);
+
+value = document.links.item(4).getAttribute("class");
+console.log("link item attribute class: " + value);
+
+value = document.links.item(4).classList[3];
+console.log("link item classList: " + value);
+
+value = document.forms.item(1).children;
+console.log("form item children: " + value);
+
+//value = document.domain; // deprecated
+
+value = document.contentType;
+console.log("document content type: " + value);
