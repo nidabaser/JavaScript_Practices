d411664f556fb2a39206a368d74da13446e14376
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-03 23:36:18 +0300

    add document object DOM : location, href, hostname, port, pathname, all

---
+++ 37.Document Objesi DOM/app.js
@@ -0,0 +1,19 @@
+//Document Objesi | DOM
+
+// console.log(window);
+
+let value;
+
+// console.log(document);
+
+value = document.location.href;
+console.log("href: " + value);
+value = document.location.hostname;
+console.log("hostname: " + value);
+value = document.location.port;
+console.log("port: " + value);
+value = document.location.pathname;
+console.log("pathname: " + value);
+
+// value = document.all; // deprecated
+
