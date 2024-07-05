64f1673e0a0c02c87c087ae79fe7c25cd4b1c168
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-05 21:44:03 +0300

    add selector examples, getElementsByClassName

--- 38.Selectors/app.js
+++ 38.Selectors/app.js
@@ -1,31 +1,41 @@
 // Selectors (Seçiciler) - Style Özellikleri
 // classname, id, tag name
 // getElementById : id'ye göre elementi yakalar
+// getElementsByClassName : Sınıfın adına göre elementi yakalar.
 
-const button = document.getElementById("todoAddButton");
+// const button = document.getElementById("todoAddButton");
 
-console.log(button);
-console.log(button.id);
-console.log(button.getAttribute("id"));
+// console.log(button);
+// console.log(button.id);
+// console.log(button.getAttribute("id"));
 
-console.log(button.className);
-console.log(button.getAttribute("class"));
+// console.log(button.className);
+// console.log(button.getAttribute("class"));
 
-console.log(button.classList);
-console.log(button.classList[3]);
+// console.log(button.classList);
+// console.log(button.classList[3]);
 
-const classListesi = button.classList;
-classListesi.forEach(function(className){
-    console.log(className);
+// const classListesi = button.classList;
+// classListesi.forEach(function(className){
+//     console.log(className);
+// })
+
+// console.log(classListesi);
+
+// let buttonText = button.textContent;
+// let buttonText2 = button.innerHTML;
+
+// console.log(buttonText);
+// console.log(buttonText2);
+
+// button.textContent = "<b>To do Ekle</b>"; // only text
+// button.innerHTML = "<b>To do Ekle</b>"; // makes bold
+
+const todoList = document.getElementsByClassName("list-group-item");
+console.log(todoList);
+
+const todoListArr = Array.from(document.getElementsByClassName("list-group-item"));
+todoListArr.forEach(function(todo){
+    console.log(todo.textContent);
 })
 
-console.log(classListesi);
-
-let buttonText = button.textContent;
-let buttonText2 = button.innerHTML;
-
-console.log(buttonText);
-console.log(buttonText2);
-
-button.textContent = "<b>To do Ekle</b>"; // only text
-button.innerHTML = "<b>To do Ekle</b>"; // makes bold
