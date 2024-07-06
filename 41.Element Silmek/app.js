e6e15ea7c4f4ab6dd02efc4e46fe1a0e22cc873c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-07 00:18:08 +0300

    add example of deleting elements

---
+++ 41.Element Silmek/app.js
@@ -0,0 +1,18 @@
+//Element Silmek
+
+const todoList = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
+// const todo1 = document.querySelector(".list-group-item");
+
+console.log(todos);
+// todos[0].remove();
+// todos[1].remove();
+// console.log(todos);
+
+// todos[todos.length-1].remove();
+
+// todo1.remove();
+
+//----------------------------------------------
+// todoList.removeChild(todos[todos.length-1]);
+// todoList.removeChild(todos[0]);
