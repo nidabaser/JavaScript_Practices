114fbe777665555f9d723d1518dba23e9f728b93
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-07 00:11:22 +0300

    add new item in todo list

--- 40.Element Oluşturma/app.js
+++ 40.Element Oluşturma/app.js
@@ -15,3 +15,31 @@ const cardBody = document.querySelectorAll(".card-body")[1];
 console.log(cardBody);
 
 cardBody.appendChild(link);
+
+/*
+<li class="list-group-item d-flex justify-content-between">Todo 3
+<a href="#" class="delete-item">
+    <i class="fa fa-remove"></i>
+</a>
+</li>
+*/
+
+const todo = document.createElement("li");
+const todoLink = document.createElement("a");
+const i  = document.createElement("i");
+
+todo.className="list-group-item d-flex justify-content-between";
+todo.innerHTML = "Todo 5";
+
+todoLink.href="#";
+todoLink.className="delete-item";
+
+i.className="fa fa-remove";
+
+todoLink.appendChild(i);
+
+todo.appendChild(todoLink);
+
+todoList.appendChild(todo);
+
+console.log(link);
