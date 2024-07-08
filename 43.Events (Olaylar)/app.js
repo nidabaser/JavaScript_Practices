dac542ae26717f3c9b45fdb7bf1982f1028a321b
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-08 23:04:09 +0300

    events, onClick examples

---
+++ 43.Events (Olaylar)/app.js
@@ -0,0 +1,26 @@
+//* Events (Olaylar)
+
+//? onClick() examples
+
+/**
+ *  <a href="#" onclick="alert('Hello!')" id="todoClearButton" 
+ *  class="btn btn-primary btn-sm mt-3">Tüm Todoları Temizle</a>
+ */
+
+/**
+ *  <a href="#" onclick="this.textContent = 'Button content changed'"
+ *  id="todoClearButton"  class="btn btn-primary btn-sm mt-3">Tüm Todoları Temizle</a>
+ */
+
+/**
+ *  <a href="#" onclick = " document.querySelectorAll('.card-title')[1].textContent='Title changed' "
+ *  id="todoClearButton"  class="btn btn-primary btn-sm mt-3">Tüm Todoları Temizle</a>
+ */
+
+
+//* using function
+function changeTitle(){
+    onClick = document.querySelectorAll('.card-title')[1].textContent='Title changed';
+    console.log("Başlık değişti");
+}
+
