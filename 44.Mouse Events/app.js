4a0473e1cf07110b87f92e8932aeeca8beb0696a
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-08 23:40:22 +0300

    mouse events, mouseover, out, enter, leave examples

--- 44.Mouse Events/app.js
+++ 44.Mouse Events/app.js
@@ -3,33 +3,47 @@
 /**
  * DOMContentLoaded
  * load
+ * 
  * click
  * dblclick
- * dblclick
+ * 
  * mouseover
  * mouseout
  * mouseenter
  * mouseleave
  */ 
 
-// // DOMContentLoaded
+// //* DOMContentLoaded
 // document.addEventListener("DOMContentLoaded",run);
 // function run(){
 //     alert("Sayfa Yüklendi");
 // }
 
-// // load
+// //* load
 // window.addEventListener("load",run);
 // function run(){
 //     console.log("Sayfa Yüklendi");
 // }
 
-const cardTitle = document.querySelectorAll(".card-title")[1];
+// //* click and dblclick
+// const cardTitle = document.querySelectorAll(".card-title")[1];
+// cardTitle.addEventListener("click", run);
+// cardTitle.addEventListener("dblclick", run);
+// function run(e){
+//     console.log(e.type);
+// }
 
-//cardTitle.addEventListener("click", run);
-cardTitle.addEventListener("dblclick", run);
+// //* mouseover, out, enter, leave
+const cardBody = document.querySelectorAll(".card-body")[1];
+
+cardBody.addEventListener("mouseover",run);
+
+cardBody.addEventListener("mouseout",run);
+
+cardBody.addEventListener("mouseenter",run);
+
+cardBody.addEventListener("mouseleave",run);
 
 function run(e){
     console.log(e.type);
 }
-
