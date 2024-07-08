d6e705fd6665d22cdb95288f431cf109605c23bd
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-08 23:32:00 +0300

    add mouse events, DOMContentLoaded and load events examples

---
+++ 44.Mouse Events/app.js
@@ -0,0 +1,26 @@
+// Mouse Events
+
+/**
+ * DOMContentLoaded
+ * load
+ * click
+ * dblclick
+ * dblclick
+ * mouseover
+ * mouseout
+ * mouseenter
+ * mouseleave
+ */ 
+
+// DOMContentLoaded
+document.addEventListener("DOMContentLoaded",run);
+function run(){
+    alert("Sayfa Yüklendi");
+}
+
+// load
+window.addEventListener("load",run);
+function run(){
+    console.log("Sayfa Yüklendi");
+}
+
