8c53e9773939431db4ec81bc09abd0b0dd9122d0
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-09 22:06:07 +0300

    add keyboard events, keypress, keydown, keyup examples with methods key, keyCode and which

--- 45.Keyboard Events/app.js
+++ 45.Keyboard Events/app.js
@@ -2,31 +2,30 @@
 
 /**
  *  keypress : Sadece harf ve sayılarda tetiklenen event
+ *  
  *  keydown : Tab, caps lock, ok tuşları, alt, ctrl gibi özel tuşlarda dahil hepsinde çalışır.
+ *  
  *  keyup : Tuştan elini kaldırdığında çalışır. Özel tuşlar dahil.
  */
 
-document.addEventListener("keypress",run);
-
-function run(e){
+document.addEventListener("keypress",run1);
+function run1(e){
     console.log(e);
     console.log("Pressed Key: " + e.key);
     console.log("Pressed Key ASCII: " + e.keyCode);
     console.log("Pressed Key ASCII: " + e.which);
 }
 
-document.addEventListener("keydown",run);
-
-function run(e){
+document.addEventListener("keydown",run2);
+function run2(e){
     console.log(e);
     console.log("Pressed Key: " + e.key);
     console.log("Pressed Key ASCII: " + e.keyCode);
     console.log("Pressed Key ASCII: " + e.which);
 }
 
-document.addEventListener("keyup",run);
-
-function run(e){
+document.addEventListener("keyup",run3);
+function run3(e){
     console.log(e);
     console.log("Key: " + e.key);
     console.log("Key ASCII: " + e.keyCode);
