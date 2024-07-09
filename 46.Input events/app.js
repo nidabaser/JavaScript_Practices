68964192b881f2f91953a70b14dcf5b74b59f82c
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-09 23:03:24 +0300

    input events, cut and select example

--- 46.Input events/app.js
+++ 46.Input events/app.js
@@ -13,8 +13,10 @@ const todo = document.querySelector("#todoName");
 
 //* todo.addEventListener("focus", run);
 //* todo.addEventListener("blur", run);
-todo.addEventListener("copy", run);
-todo.addEventListener("paste", run);
+//* todo.addEventListener("copy", run);
+//* todo.addEventListener("paste", run);
+todo.addEventListener("cut", run);
+todo.addEventListener("select", run);
 
 function run(e) {
     console.log(e.type);
