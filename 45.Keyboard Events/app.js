a91e766d21076e973a96f9707f7865abccd3dee0
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-09 22:16:20 +0300

    prevent a user from refreshing the page by pressing F5

--- 45.Keyboard Events/app.js
+++ 45.Keyboard Events/app.js
@@ -8,6 +8,7 @@
  *  keyup : Tuştan elini kaldırdığında çalışır. Özel tuşlar dahil.
  */
 
+/*
 document.addEventListener("keypress",run1);
 function run1(e){
     console.log(e);
@@ -30,4 +31,16 @@ function run3(e){
     console.log("Key: " + e.key);
     console.log("Key ASCII: " + e.keyCode);
     console.log("Key ASCII: " + e.which);
+}
+*/
+
+// How to prevent a user from refreshing the page by pressing F5
+document.addEventListener("keydown",runF5);
+function runF5(e){
+    console.log("Key: " + e.key);
+    console.log("Key ASCII: " + e.keyCode);
+    if(e.keyCode == 116){
+        alert("Refreshing page is prevented");
+    }
+    e.preventDefault();
 }
