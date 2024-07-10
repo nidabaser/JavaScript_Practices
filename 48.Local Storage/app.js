f56a1ea28bb06476073944ee5c3dc4e228a03ee5
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-11 01:32:13 +0300

    localStorage, getItem, setItem, JSON.stringify, JSON.parse, forEach

--- 48.Local Storage/app.js
+++ 48.Local Storage/app.js
@@ -1,12 +1,28 @@
 // Local Storage
 
-localStorage.setItem("motion1","Push Up");
-localStorage.setItem("motion2","Barfix");
-localStorage.setItem("motion3","Squat");
+// localStorage.setItem("motion1","Push Up");
+// localStorage.setItem("motion2","Barfix");
+// localStorage.setItem("motion3","Squat");
 
-let value = localStorage.getItem("motion1");
-console.log(value);
+// let value = localStorage.getItem("motion1");
+// console.log(value);
 
-localStorage.removeItem("motion2");
+// localStorage.removeItem("motion2");
 
-localStorage.clear();
+// localStorage.clear();
+
+let motions = ["Push up", "Barfix", "Squat", "Chin up"];
+
+localStorage.setItem("motions", JSON.stringify(motions));
+
+let value = JSON.parse(localStorage.getItem("motions"));
+
+//console.log(value);
+
+value.forEach(function(motion){
+    console.log(motion);
+})
+
+//* Tarayıcıyı açıp kapattığımızda
+//* Session Storage'dan temizlenen veriler
+//* Local Storage'dan temizlenmezler.
