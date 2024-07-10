7b5d9dbed1e26ecba0bd62c308d3b845cf5b9aaf
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-10 18:56:30 +0300

    add value in session storage

--- 47.Session Storage/app.js
+++ 47.Session Storage/app.js
@@ -3,3 +3,8 @@ console.log(window);
 console.log(window.sessionStorage);
 console.log(sessionStorage);
 
+// Adding Value (Key, Value)
+sessionStorage.setItem("123", "Nida");
+sessionStorage.setItem("456", "Sena");
+sessionStorage.setItem("789", "Adin");
+sessionStorage.setItem("101", "Anes");
