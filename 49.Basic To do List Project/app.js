aa815afcd95bf606d83e6c37218c3168f506ebe5
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-16 00:06:13 +0300

    add todolist project | select all elements

---
+++ 49.Basic To do List Project/app.js
@@ -0,0 +1,9 @@
+//Tüm Elementleri Seçmek
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+const filterInput = document.querySelector("#todoSearch");
