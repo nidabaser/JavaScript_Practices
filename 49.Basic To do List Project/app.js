806816cb2dd2b947926496c2261ca6227de6d8f0
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-19 23:45:57 +0300

    add runEvents and addTodo events | using addEventListener and preventDefault

--- 49.Basic To do List Project/app.js
+++ 49.Basic To do List Project/app.js
@@ -7,3 +7,14 @@ const firstCardBody = document.querySelectorAll(".card-body")[0];
 const secondCardBody = document.querySelectorAll(".card-body")[1];
 const clearButton = document.querySelector("#clearButton");
 const filterInput = document.querySelector("#todoSearch");
+
+runEvents();
+
+function runEvents(){
+    form.addEventListener("submit",addTodo);
+}
+
+function addTodo(e){
+    console.log("Submit event'i çalıştı");
+    e.preventDefault();
+}
