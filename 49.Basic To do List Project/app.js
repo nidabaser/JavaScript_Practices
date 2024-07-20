3f3210bde17edce5156bc4dcb12a4d90b19d86be
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-20 20:48:08 +0300

    add new methods and finish todo project | I love backend but I dont like frontend...

--- 49.Basic To do List Project/app.js
+++ 49.Basic To do List Project/app.js
@@ -8,25 +8,100 @@ const secondCardBody = document.querySelectorAll(".card-body")[1];
 const clearButton = document.querySelector("#clearButton");
 const filterInput = document.querySelector("#todoSearch");
 
+let todos = [];
+
 runEvents();
 
-function runEvents(){
-    form.addEventListener("submit",addTodo);
+function runEvents() {
+    form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded",pageLoaded);
+    secondCardBody.addEventListener("click",removeTodoToUI);
+    clearButton.addEventListener("click",allTodosEverywhere);
+    filterInput.addEventListener("keyup",filter);
 }
 
-function addTodo(e){
-    const inputText = addInput.value.trim();
-    if(inputText == null || inputText==""){
-        alert("Boş olamaz!");
-    } else {
-        addTodoUI(inputText);
-        addTodoToStorage(inputText);
+function pageLoaded(){
+    checkTodosFromStorage();
+    todos.forEach(function(todo){
+       addTodoToUI(todo);
+    });
+}
+
+function filter(e){
+    const filterValue = e.target.value.toLowerCase().trim();
+    const todoListesi = document.querySelectorAll(".list-group-item");
+    
+    if(todoListesi.length>0){
+        todoListesi.forEach(function(todo){
+            if(todo.textContent.toLowerCase().trim().includes(filterValue)){
+                //
+                todo.setAttribute("style","display : block");
+            }else{
+                todo.setAttribute("style","display : none !important");
+            }
+        });
+
+    }else{
+        showAlert("warning","Filtreleme yapmak için en az bir todo olmalıdır!");
     }
+
+}
+
+function allTodosEverywhere(){
+   const todoListesi = document.querySelectorAll(".list-group-item");
+   if(todoListesi.length>0){
+    //Ekrandan Silme
+    todoListesi.forEach(function(todo){
+        todo.remove();
+    });
+
+    //Storage'dan Silme
+    todos=[];
+    localStorage.setItem("todos",JSON.stringify(todos));
+    showAlert("success","Başarılı bir şekilde silindi");
+   }else{
+    showAlert("warning","Silmek için en az bir todo olmalıdır");
+   }
+}
+
+function removeTodoToUI(e){
+    if(e.target.className==="fa fa-remove"){
+        //Ekrandan Silme
+       const todo = e.target.parentElement.parentElement;
+       todo.remove();
+
+       //Storage'dan Silme
+       removeTodoToStorage(todo.textContent);
+       showAlert("success","Todo başarıyla silindi.");
+    }
+}
+
+function removeTodoToStorage(removeTodo){
+    checkTodosFromStorage();
+    todos.forEach(function(todo,index){
+        if(removeTodo===todo){
+            todos.splice(index,1);
+        }
+    });
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function addTodo(e) {
+    const inputText = addInput.value.trim();
+    if (inputText == null || inputText == "") {
+        showAlert("warning", "Lütfen boş bırakmayınız!");
+    } else {
+        //Arayüze ekleme
+        addTodoToUI(inputText);
+        addTodoToStorage(inputText);
+        showAlert("success", "Todo Eklendi.");
+    }
+
+    //storage ekleme
     e.preventDefault();
 }
 
-// Arayüze todo ekleme metodu
-function addTodoUI(newTodo){
+function addTodoToUI(newTodo) {
     const li = document.createElement("li");
     li.className = "list-group-item d-flex justify-content-between";
     li.textContent = newTodo;
@@ -45,9 +120,8 @@ function addTodoUI(newTodo){
     addInput.value = "";
 }
 
-// Storage'a todo ekleme metodu
 function addTodoToStorage(newTodo) {
-    checkTodosFromStorage(); // Different method call for null check
+    checkTodosFromStorage();
     todos.push(newTodo);
     localStorage.setItem("todos", JSON.stringify(todos));
 }
@@ -58,4 +132,17 @@ function checkTodosFromStorage() {
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
+}
+
+function showAlert(type, message) {
+    const div = document.createElement("div");
+    //   div.className="alert alert-"+type;
+    div.className = `alert alert-${type}`; //litirel template
+    div.textContent = message;
+
+    firstCardBody.appendChild(div);
+
+    setTimeout(function(){
+        div.remove();
+    },2500);
 }
