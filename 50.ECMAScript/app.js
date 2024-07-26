65a81aeb2c5d570f2ca44e7e4178c5874edec45b
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-07-26 14:58:26 +0300

    intro to ECMAScript

---
+++ 50.ECMAScript/app.js
@@ -0,0 +1,58 @@
+// ECMAScript
+
+/**
+ * ECMAScript ile JavaScript aynı şey değil..!
+ * ECMAScript betik dilleri için bir standart
+ * Yani yazdığımız JS kodunun hem chrome, hem edge, 
+ * hem de diğer tüm tarayıcılarda çalışmasını sağlamak içindir.
+ * Bir kere yazıp bütün tarayıcılarda çalışmasını sağlamak için yani.
+ * 
+ * ECMAScript sanki JS'in version farklılıklarının kontrolü yeni eklenmiş özellikler gibi düşünülebilir.
+ * 
+ * Kaynak: https://www.w3schools.com/js/js_versions.asp
+ * 
+ * ES6	ECMAScript 2015 ile hayatımıza giren kavramlar şunlardır:
+    
+    New Features in ES6:
+
+        Added let and const
+        Added default parameter values
+        Added Array.find()
+        Added Array.findIndex()
+        The let keyword
+        The const keyword
+        Arrow Functions
+        The {a,b} = Operator
+        The [a,b] = Operator
+        The ... Operator
+        For/of
+        Map Objects
+        Set Objects
+        Classes
+        Promises*** (callback cehenneminden çıkış :D )
+        Symbol
+        Default Parameters
+        Function Rest Parameter
+        String.includes()
+        String.startsWith()
+        String.endsWith()
+        Array entries()
+        Array.from()
+        Array keys()
+        Array find()
+        Array findIndex()
+        Math.trunc
+        Math.sign
+        Math.cbrt
+        Math.log2
+        Math.log10
+        Number.EPSILON
+        Number.MIN_SAFE_INTEGER
+        Number.MAX_SAFE_INTEGER
+        Number.isInteger()
+        Number.isSafeInteger()
+        New Global Methods
+        JavaScript Modules
+
+ * Kaynak: https://www.w3schools.com/js/js_es6.asp       
+ */
