253aa4fc50cf7dc549e6eed67272dcf0acdb2d0d
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-08-01 22:59:51 +0300

    Arrow function simplification

--- 51.Arrow Functions/app.js
+++ 51.Arrow Functions/app.js
@@ -14,3 +14,15 @@ const yazdirParam2 = (firstName, lastName) => {
     console.log("Selam", firstName, lastName)
 }
 yazdirParam2("Alice","Dove");
+
+// Arrow function sadeleştirmek
+const kareAl = (x)=>{
+    return x*x
+}
+console.log("Hesaplanan değer: ", kareAl(3));
+
+// Yukarıdaki arrow fonksiyonu şu şekilde sadeleştirilebilir:
+const kupAl = x=>x*x*x
+console.log("Sadeleştirilmiş Arrow Fonksiyonu ile hesaplanan değer: ", kupAl(3));
+
+// Not: Yani sadece parametre, ok ve return kısmı da yeterli aslında bir değişkene eşitleyip kullanıyoruz ona bile gerek yok
