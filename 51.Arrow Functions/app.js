11a8d3fda4a5103555d547dcfbe8c14ef950b0d4
Nida Başer <97883562+nidabaser@users.noreply.github.com>
2024-08-01 22:50:28 +0300

    add arrow functions with and without parameters

---
+++ 51.Arrow Functions/app.js
@@ -0,0 +1,16 @@
+// Arrow Functions
+
+const yazdir = () => {
+    console.log("Selam")
+}
+yazdir();
+
+const yazdirParam = (firstName) => {
+    console.log("Selam", firstName)
+}
+yazdirParam("Alice");
+
+const yazdirParam2 = (firstName, lastName) => {
+    console.log("Selam", firstName, lastName)
+}
+yazdirParam2("Alice","Dove");
